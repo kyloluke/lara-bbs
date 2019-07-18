936ce2401f4dc11f379af2675830cd4e68fd081f
vagrant <vagrant@homestead>
2019-07-18 11:48:20 +0000

    easysms install and configure

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function ($api) {
-    $api->get('version', function () {
-        return response('this is version v1');
-    });
-});
+// $api->version('v1', function ($api) {
+//     $api->get('version', function () {
+//         return response('this is version v1');
+//     });
+// });
 
-$api->version('v2', function ($api) {
-    $api->get('version', function () {
-        return response('this is version v2');
-    });
-});
+// $api->version('v2', function ($api) {
+//     $api->get('version', function () {
+//         return response('this is version v2');
+//     });
+// });
